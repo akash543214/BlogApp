1d896489ebe00a1bb9c5b4997cef121f964140d6
akash543214 <sharma.54akash@gmail.com>
2024-01-09 16:43:50 +0530

    issue solved

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -57,7 +57,7 @@ export class AuthService {
     async googleAuth() {
 
         try {
-           return await this.account.createOAuth2Session('google', 'https://celadon-griffin-fbffd2.netlify.app', 'https://celadon-griffin-fbffd2.netlify.app/login');
+           return await this.account.createOAuth2Session('google', 'https://celadon-griffin-fbffd2.netlify.app/', 'https://cloud.appwrite.io/v1/account/sessions/oauth2/callback/google/655cb434ba462e6c01ed');
 
         } catch (error) {
             console.log("Appwrite serive :: logout :: error", error);
